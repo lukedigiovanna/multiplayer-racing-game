d27f0251bde3ac89d350c7c86383faa766bd428e
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2024-03-15 20:13:11 -0400

    project skeleton

---
+++ run_server.bat
@@ -0,0 +1,7 @@
+:: Runs the server, assuming it has been built
+
+@echo off
+cd build/server
+GameServer.exe
+echo.
+pause > nul
---
+++ server/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, server\n";
+
+    return 0;
+}
